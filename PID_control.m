d5f106d711bf8432c3f2e90eb41dbde5640aef49
GiorgeGi <97761991+GiorgeGi@users.noreply.github.com>
2023-07-13 20:16:45 +0300

    Update PID_control.m
    Added loops for testing multiple sets of gain values without repeatedly  executing the script.

--- PID_control.m
+++ PID_control.m
@@ -24,81 +24,127 @@ step(M, 'p--b');            % Plot the step response of the closed-loop system M
 
 hold on;                    % Allow multiple plots on the same figure
 
-%line([0, 100], [1, 1], 'Color', 'red', 'LineStyle', '--', 'LineWidth', 2);       % In Octave the transfer function is not getting plotted
-                                                                                  % Uncomment this to draw a line similar to the transfer function in Octave
-
 
 
 choice = input("Choose P (1), PI (2), PD (3), or PID (4): ");
+fprintf("\n");
 switch choice
   
   case 1
+  
+    stop = 1;
+    while(stop == true)
+      
+      Kp = input("Enter the value of the proportional gain Kp: ");
     
-    Kp = input("Enter the value of the proportional gain Kp: ");
+      % Create a PID controller object with the specified gain
+      % GC = Kp + Ki*(1/s) + Kd*s
+      GC = pid(Kp);  
+      % Create a new closed-loop transfer function connecting the system GC*S and the feedback path H    
+      MC = feedback(GC*S, H);
+      % Plot the step response of the closed-loop system M using a dashed blue line
+      step(M, 'p--b'); 
+      % Allow multiple plots on the same figure
+      hold on;
+      % Plot the step response
+      step(MC, '*-.g');
+      % In Octave the transfer function is not getting plotted
+      % Uncomment the following to draw a line similar to the transfer function in Octave
+      %line([0, 100000], [1, 1], 'Color', 'red', 'LineStyle', '--', 'LineWidth', 2);
+ 
     
-    % Create a PID controller object with the specified gain
-    % GC = Kp + Ki*(1/s) + Kd*s
-    GC = pid(Kp);   
-    % Create a new closed-loop transfer function connecting the system GC*S and the feedback path H    
-    MC = feedback(GC*S, H); 
-    % Plot the step response
-    step(MC, '*-.g');
-    
-    
-    % Initiallize graph grid
-    grid on;
-    % Create a label to hold information
-    legend({strcat("Open Loop"), strcat("Kp=", num2str(Kp))}, "location", "southeast");
-    % By design legends are enclosed in a box, remove it
-    legend boxoff;
-    % Graph title
-    title("Proportional Controller");
+      % Initiallize graph grid
+      grid on;
+      % Create a label to hold information
+      legend({strcat("Open Loop"), strcat("Kp=", num2str(Kp))}, "location", "southeast");
+      % By design legends are enclosed in a box, remove it
+      legend boxoff;
+      % Graph title
+      title("Proportional Controller");
+      
+      fprintf("\n");
+      stop = input("End? Yes (0) or No (1): ");
+      fprintf("\n");
+    end
     
   case 2
+    
+    stop = 1;
+    while(stop == true)
   
-    Kp = input("Enter the value of the proportional gain Kp: ");
-    Ki = input("Enter the value of the integral gain Ki: ");
+      Kp = input("Enter the value of the proportional gain Kp: ");
+      Ki = input("Enter the value of the integral gain Ki: ");
     
-    GC = pid(Kp, Ki);
-    MC = feedback(GC*S, H);
-    step(MC, '*-.g');
+      GC = pid(Kp, Ki);
+      MC = feedback(GC*S, H);
+      step(M, 'p--b'); 
+      hold on;
+      step(MC, '*-.g');
+      %line([0, 100000], [1, 1], 'Color', 'red', 'LineStyle', '--', 'LineWidth', 2);
     
     
-    grid on;
-    legend({strcat("Open Loop"), strcat("Kp=", num2str(Kp), ", Ki=", num2str(Ki))}, "location", "southeast");
-    legend boxoff;
-    title("Proportional Integral Controller");
+      grid on;
+      legend({strcat("Open Loop"), strcat("Kp=", num2str(Kp), ", Ki=", num2str(Ki))}, "location", "southeast");
+      legend boxoff;
+      title("Proportional Integral Controller");
+    
+      fprintf("\n");
+      stop = input("End? Yes (0) or No (1): ");
+      fprintf("\n");
+    end
     
   case 3
   
-    Kp = input("Enter the value of the proportional gain Kp: ");
-    Kd = input("Enter the value of the derivative gain Kd: ");
+    stop = 1;
+    while(stop == true)
+  
+      Kp = input("Enter the value of the proportional gain Kp: ");
+      Kd = input("Enter the value of the derivative gain Kd: ");
     
-    GC = pid(Kp, Kd);
-    MC = feedback(GC*S, H);
-    step(MC, '*-.g');
+      GC = pid(Kp, Kd);
+      MC = feedback(GC*S, H);
+      step(M, 'p--b'); 
+      hold on;
+      step(MC, '*-.g');
+      %line([0, 100000], [1, 1], 'Color', 'red', 'LineStyle', '--', 'LineWidth', 2);
     
     
-    grid on;
-    legend({strcat("Open Loop"), strcat("Kp=", num2str(Kp), ", Kd=", num2str(Kd))}, "location", "southeast");
-    legend boxoff;
-    title("Proportional Derivative Controller");
+      grid on;
+      legend({strcat("Open Loop"), strcat("Kp=", num2str(Kp), ", Kd=", num2str(Kd))}, "location", "southeast");
+      legend boxoff;
+      title("Proportional Derivative Controller");
+    
+      fprintf("\n");
+      stop = input("End? Yes (0) or No (1): ");
+      fprintf("\n");
+    end
     
   case 4
   
-    Kp = input("Enter the value of the proportional gain Kp: ");
-    Ki = input("Enter the value of the integral gain Ki: ");
-    Kd = input("Enter the value of the derivative gain Kd: ");
+    stop = 1;
+    while(stop == true)
     
-    GC = pid(Kp, Ki, Kd);
-    MC = feedback(GC*S, H);
-    step(MC, '*-.g');
+      Kp = input("Enter the value of the proportional gain Kp: ");
+      Ki = input("Enter the value of the integral gain Ki: ");
+      Kd = input("Enter the value of the derivative gain Kd: ");
+    
+      GC = pid(Kp, Ki, Kd);
+      MC = feedback(GC*S, H);
+      step(M, 'p--b'); 
+      hold on;
+      step(MC, '*-.g');
+      %line([0, 100000], [1, 1], 'Color', 'red', 'LineStyle', '--', 'LineWidth', 2);
     
     
-    grid on;
-    legend({strcat("Open Loop"), strcat("Kp=", num2str(Kp), ", Ki=", num2str(Ki), ", Kd=", num2str(Kd))}, "location", "southeast");
-    legend boxoff;
-    title("Proportional Integral Derivative Controller");
+      grid on;
+      legend({strcat("Open Loop"), strcat("Kp=", num2str(Kp), ", Ki=", num2str(Ki), ", Kd=", num2str(Kd))}, "location", "southeast");
+      legend boxoff;
+      title("Proportional Integral Derivative Controller");
+      
+      fprintf("\n");
+      stop = input("End? Yes (0) or No (1): ");
+      fprintf("\n");
+    end
   
   otherwise
     
